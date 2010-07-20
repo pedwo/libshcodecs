590b81c9bb8c19f37afd36941094e858dd92f0a0
Phil Edworthy <phil.edworthy@renesas.com>
2010-07-20 16:25:51 +0100

    dec: Fix included headers for debug messages
    Also remove header files that aren't used.

--- src/libshcodecs/shcodecs_decoder.c
+++ src/libshcodecs/shcodecs_decoder.c
@@ -21,16 +21,12 @@
 #include "config.h"
 #endif
 
-#include <fcntl.h>
 #include <stdio.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
-#include <getopt.h>
-#include <sched.h>
 #include <unistd.h>
 #include <sys/param.h>
-#include <sys/types.h>
-#include <sys/time.h>
 #include "shcodecs/shcodecs_decoder.h"
 #include "avcbd.h"
 #include "avcbd_optionaldata.h"
